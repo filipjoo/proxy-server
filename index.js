6237871098f7291d5e7a2c7dd567da210f809f0e
filipjoo <filipjoo@SN3_PC>
2023-01-03 03:08:01 +0900

    wetherAPI対応

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const { createProxyMiddleware } = require("http-proxy-middleware")
 const rateLimit = require("express-rate-limit")
 require("dotenv").config()
+const url = require("url")
 
 const limiter = rateLimit({
     windowMs: 15 *60 *1000,
@@ -14,11 +15,22 @@ app.get("/", (req, res) => {
 })
 
 app.use("/corona-tracker-world-data", limiter, (req, res, next) => {
+    const city = url.parse(req.url).query
     createProxyMiddleware({
         target: process.env.BASE_API_URL_CORONA_WORLD,
         changeOrigin: true,
         pathRewrite: {
             [`^/corona-tracker-world-data`]: "",
+        },
+    }) (req, res, next)
+})
+
+app.use("/wether-data", limiter, (req, res, next) => {
+    createProxyMiddleware({
+        target: "${process.env.BASE_API_URL_WEATHERAPI}${city}&api=no",
+        changeOrigin: true,
+        pathRewrite: {
+            [`^/wether-data`]: "",
         },
     }) (req, res, next)
 })
